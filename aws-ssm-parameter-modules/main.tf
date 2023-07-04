00d1aa6eec3fdfce699d6125a2ebf305b5e1db6b
tusharkumar <tusharkumar27864@gmail.com>
2023-07-04 17:39:11 +0530

    code updated

--- aws-ssm-parameter-modules/main.tf
+++ aws-ssm-parameter-modules/main.tf
@@ -4,7 +4,6 @@ resource "aws_ssm_parameter" "my_parameter" {
   value           = var.value
   description     = var.description
   tier            = var.tier
-  key_id          = var.key_id
   overwrite       = var.overwrite
   allowed_pattern = var.allowed_pattern
   data_type       = var.data_type
